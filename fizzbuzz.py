17a2488c182280a0ef4c5e162d92acee78a51c3c
kanishkgarga <135623563+kanishkgarga@users.noreply.github.com>
2023-06-06 19:07:43 +0530

    my python practise

---
+++ fizzbuzz.py
@@ -0,0 +1,28 @@
+# the fizz buzz problem
+
+for fizzbuzz in range(1,51):
+    if fizzbuzz%3==0 and fizzbuzz%5==0:
+        print("fizzbuzz")
+
+    if fizzbuzz%3==0:
+        print("fizz")
+        continue
+    if fizzbuzz%5==0:
+        print("buzz")
+        continue
+
+    
+    print(fizzbuzz)    
+    
+
+
+
+
+
+
+
+
+
+
+
+
