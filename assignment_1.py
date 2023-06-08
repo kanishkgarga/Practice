9942a0600c16deab47451e749376d8fda9df5269
kanishkgarga <135623563+kanishkgarga@users.noreply.github.com>
2023-06-08 12:29:58 +0530

    Assignment_1 Python

---
+++ assignment_1.py
@@ -0,0 +1,111 @@
+# Ans1- strings-->1.used to represent sequence of charcters
+# 2.specific type of Data
+# 3.they are immutable
+
+# variable-->1.its like a container for storing various data including strings
+# 2.they are mutable
+
+
+# Ans2- Three Diffrent types of data types are as follow
+
+# -numeric data type --> which let us represent whole numbers
+# eg--> int, float,etc
+
+
+# -string-->it represents a sequence of characters enclosed in single or double qoutes
+
+# -boolean-->represent the true value of an expression. it can have two possible values true or false
+
+# Ans3-the bacon varible remains 22 as to change value of bacon 
+# this expression is required bacon=bacon+1
+
+
+
+
+
+# Ans4- 1. spamspamspam
+# 2.spamspamspam
+
+# Ans5-Because a valid varible name always start with a letter
+    
+# Ans6-this expression cuases an error beacuse you cant concatinate a integer with strings 
+
+# --> to solve this we can put single or double qoutes around 99 then the expression would work
+
+# Ans7-spam=int(input("enter any value="))
+
+# if (spam==1):
+#     print("hello")
+# if (spam==2):
+#     print("howdy")
+# else:
+#     print("Greetings!")
+
+
+
+
+# Ans8-break statement brings you out of the loop as soon as the condition required comesss
+
+# wheares continue statement only stop the ongoing iteration of the Loop
+
+
+# Ans9-in range(10) the starting point is taken 0 by defualt so there is no diffrence between range(10)
+# and range(0,10) wheareas in range(0,10,2 ) here 2 is the step the number of spaces between the sequence.
+
+# Ans10-  1. for i in range(1,11):
+#             print(i)
+
+
+# 2.i=1
+# while(i<=10):
+#         print(i)
+
+# Ans11-
+
+
+
+
+# Ans12- it is data type of the class Nonetype
+
+# Ans13- the purpose of try clause is that it puts all the code under code in check as soon as the program under try
+# gives an error instead of stoping the program it directs towards whatever is written in except
+
+# Ans 14- these are basically sqaure brackets ,they are used to define list data structures in python,they are also used for indexing etc
+
+# Ans15-spam.insert(2,'hello')
+
+
+
+# Ans16-1.value will be 'd'
+# 2.value will be 'd'
+# 3.a,b
+
+# Ans 17-1. the index will be 1
+# 2.the list will look like following[3.14,'cat'.11,'cat',true,99]
+# 3.the list will look like followin[3.14,11,true,99]
+
+# Ans18-in append whatever we add goes to the end of the list
+# wheares in insert we can specify at which index we need to insert the element
+
+# Ans19-two methods are remove()and pop().
+
+# Ans20- diffrences are as follow
+# 1.list are enclosed btw sqaure brackets[]
+# tupes are enclosed btw round brakcts ()
+
+# 2.list are mutable
+# tuples are imutable
+
+# Ans21- 
+
+
+# Ans22-in copy.copy a copy of original object is stored and only refrence is copied
+# wheares in deep copy in this orignal copies and repetative copies both are stored
+
+# Ans23-empty dict={}
+
+# Ans24-the most significant diffrence is in list we use sqaure brackets [],wheares in dict we use curly brackets {}
+
+# # Ans25-we will get akey error as that key is not present in dict
+
+# Ans26-
